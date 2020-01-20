67125a208a9ec6a2b3b7883eb776d0889172eb12
Makwana Ravikumar c <52538898+Ravi1804makwana@users.noreply.github.com>
2020-01-20 12:50:18 +0530

    Practical2

---
+++ DotNetProjects/Practical2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practical2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String name, country;
+            Console.Write("Please enter the your name : ");
+            name = Console.ReadLine();
+            Console.Write("Hello !, Where are you frome ? ");
+            country = Console.ReadLine();
+            Console.WriteLine("Hello "+name+", you are from "+country+".");
+            Console.Read();
+        }
+    }
+}
